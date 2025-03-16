e4a21550af73c4471e504eedc8d8791062ea6c7d
Benjamin Gunnels <bengunnels8@gmail.com>
2025-03-16 15:29:25 -0700

    Added scale factor

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
 from .simulator.StreamSimulator import StreamSimulator
 from .simulator.RandomizedSimulator import RandomizedSimulator
 
-number_frames = 100
-moving_speed = 0.02
-frame_rate = 10
-chaos = 7
+number_frames = 240
+moving_speed = 0.04
+frame_rate = 14
+chaos = 20
 sim_name = f"stream_chaos{chaos}"
 
 
 # Terrain options = grass, sand, rock, clay
-sim = StreamSimulator(number_frames=number_frames, moving_speed=moving_speed, frame_rate=frame_rate, chaos=chaos, sim_name=sim_name, terrain="sand")
+sim = StreamSimulator(number_frames=number_frames, moving_speed=moving_speed, frame_rate=frame_rate, chaos=chaos, sim_name=sim_name, terrain="rock")
 # sim = RandomizedSimulator(number_frames=number_frames, moving_speed=moving_speed, frame_rate=frame_rate, chaos=chaos, sim_name=sim_name, terrain="clay")
 
 sim.run()
