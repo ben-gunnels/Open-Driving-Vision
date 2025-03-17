c6b60c73383e0494210ca9952f73eab3ec7cdb7c
Benjamin Gunnels <bengunnels8@gmail.com>
2025-03-16 19:05:09 -0700

    Changed sign dimensions

--- src/generators/RoadSignBuilds.py
+++ src/generators/RoadSignBuilds.py
@@ -136,9 +136,10 @@ class Builder():
             (x+MINIMUM_POLE_WIDTH*SCALE_FACTOR, y-MINIMUM_POLE_HEIGHT*SCALE_FACTOR), 
             (x, y-MINIMUM_POLE_HEIGHT*SCALE_FACTOR)
         )
+    
     def build_small_pole(self, starting_point: tuple):
         """
-        This is the pole my gf rides on
+            Reduced height of a pole for shorter objects e.g. mile marker.
         """
         x,y = starting_point
         return (
@@ -150,7 +151,8 @@ class Builder():
     
     def build_mile_marker(self, starting_point: tuple):
         """
-        this some shit
+            Builds a rectangular mile marker topper. 
+            1u wide, 0.8u tall
         """
         x,y = starting_point
         y += 5*SCALE_FACTOR
@@ -160,44 +162,7 @@ class Builder():
             (x-1*SCALE_FACTOR, y-8*SCALE_FACTOR),
             (x-1*SCALE_FACTOR, y)
         )
-    def build_tourist_help_sign(self, starting_point: tuple):
-        """
-        this for the bitches
-        """
-        x, y = starting_point
-        y += 5*SCALE_FACTOR
-        return (
-            (x+15*SCALE_FACTOR, y), 
-            (x+15*SCALE_FACTOR, y-16*SCALE_FACTOR),
-            (x-15*SCALE_FACTOR, y-16*SCALE_FACTOR), 
-            (x-15*SCALE_FACTOR, y)
-        )  
-    def build_info_sign(self, starting_point: tuple):
-        """
-        what's for dinner
-        """
-        x, y = starting_point
-        y += 5*SCALE_FACTOR
-        return (
-            (x+15*SCALE_FACTOR, y), 
-            (x+15*SCALE_FACTOR, y-16*SCALE_FACTOR),
-            (x-15*SCALE_FACTOR, y-16*SCALE_FACTOR), 
-            (x-15*SCALE_FACTOR, y)
-        )
-    def build_road_work_sign(self, starting_point: tuple):
-        """
-        OH fuck there is road work. I'm gonna be late. I should grab Starbucks
-        """
-        x, y = starting_point
-        y += 5*SCALE_FACTOR
-        return (
-            (x, y), 
-            (x+5*SCALE_FACTOR, y-5*SCALE_FACTOR), 
-            (x, y-10*SCALE_FACTOR),
-            (x-5*SCALE_FACTOR, y-5*SCALE_FACTOR)
-        )
 
-        
     def build_diamond_sign(self, starting_point: tuple):
         """
             Builds a diamond sign shape from a starting point which is the middle of the top of the sign pole.
@@ -299,6 +264,49 @@ class Builder():
             (x+1.8*SCALE_FACTOR, y-11.2*SCALE_FACTOR), 
             (x-1.8*SCALE_FACTOR, y-11.2*SCALE_FACTOR), 
             (x-6.5*SCALE_FACTOR, y-4.7*SCALE_FACTOR)
+        )
+    
+    def build_tourist_help_sign(self, starting_point: tuple):
+        """
+            Rectangular sign for tourist help information.
+            1.5u wide, 1.4u tall
+        """
+        x, y = starting_point
+        y += 5*SCALE_FACTOR
+        return (
+            (x+15*SCALE_FACTOR, y), 
+            (x+15*SCALE_FACTOR, y-14*SCALE_FACTOR),
+            (x-15*SCALE_FACTOR, y-14*SCALE_FACTOR), 
+            (x-15*SCALE_FACTOR, y)
+        )  
+    
+    def build_info_sign(self, starting_point: tuple):
+        """
+            Rectangular information sign.
+            1.2u wide, 1.1u tall
+
+        """
+        x, y = starting_point
+        y += 5*SCALE_FACTOR
+        return (
+            (x+12*SCALE_FACTOR, y), 
+            (x+12*SCALE_FACTOR, y-11*SCALE_FACTOR),
+            (x-12*SCALE_FACTOR, y-11*SCALE_FACTOR), 
+            (x-12*SCALE_FACTOR, y)
+        )
+    
+    def build_road_work_sign(self, starting_point: tuple):
+        """
+            Diamond sign for road work information.
+            1.2u wide, 1.2u tall
+        """
+        x, y = starting_point
+        y += 5*SCALE_FACTOR
+        return (
+            (x, y), 
+            (x+6*SCALE_FACTOR, y-6*SCALE_FACTOR), 
+            (x, y-12*SCALE_FACTOR),
+            (x-6*SCALE_FACTOR, y-6*SCALE_FACTOR)
         )
     
     def build_traffic_cone(self, starting_point: tuple, anchor="left"):
