3fca2289affd1f2a48d04d6e32e6e80aa9be602e
ben-gunnels <97767157+ben-gunnels@users.noreply.github.com>
2025-03-21 22:47:26 -0700

    Added documentation

--- src/app.py
+++ src/app.py
@@ -1,6 +1,19 @@
 from .simulator.StreamSimulator import StreamSimulator
 from .simulator.RandomizedSimulator import RandomizedSimulator
 
+"""
+  Edit these parameters to adjust the driving experience.
+  number_frames specifies the number of images produced by the simulator.
+  moving_speed specifies the rate at which objects move relative to their perceived size.
+  frame_rate determines video playback rate. Higher frame rate results in faster video for a given moving speed.
+  chaos is a multiplier that causes more objects to spawn on the screen.
+  sim_name defines the video output name for the file. 
+
+  StreamSimulator simulates a continuous driving experience.
+  RandomizedSimulator generates random images of roadside objects. This is ideal for training a model on novel images.
+"""
+
+# Key parameters
 number_frames = 150
 moving_speed = 0.02 # Keep around 0.02
 frame_rate = 15
@@ -10,6 +23,8 @@ sim_name = f"stream_chaos_{chaos}"
 
 # Terrain options = grass, sand, rock, clay
 sim = StreamSimulator(number_frames=number_frames, moving_speed=moving_speed, frame_rate=frame_rate, chaos=chaos, sim_name=sim_name, terrain="clay")
+
+# Additional terrain option = random
 # sim = RandomizedSimulator(number_frames=number_frames, moving_speed=moving_speed, frame_rate=frame_rate, chaos=chaos, sim_name=sim_name, terrain="clay")
 
 sim.run()
