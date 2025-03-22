2e537acc43777eb292f4c5af7d08e6cc5c8544bf
Benjamin Gunnels <bengunnels8@gmail.com>
2025-03-22 11:25:08 -0700

    Improvements to app

--- src/app.py
+++ src/app.py
@@ -25,9 +25,7 @@ sim_name = f"{mode}_chaos_{chaos}"
 # Terrain options = grass, sand, rock, clay
 if mode == "stream":
     sim = StreamSimulator(number_frames=number_frames, moving_speed=moving_speed, frame_rate=frame_rate, chaos=chaos, sim_name=sim_name, terrain=terrain)
-
 else:
-    sim = RandomizedSimulator(number_frames=number_frames, moving_speed=moving_speed, frame_rate=frame_rate, chaos=chaos, sim_name=sim_name, terrain="clay")
-
+    sim = RandomizedSimulator(number_frames=number_frames, moving_speed=moving_speed, frame_rate=frame_rate, chaos=chaos, sim_name=sim_name, terrain=terrain)
 if sim:
     sim.run()
